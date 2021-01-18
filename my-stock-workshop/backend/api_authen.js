9eaec8c9d7250424584316a37c187c0589e54f4a
Kachapot <75522599+Kachapot@users.noreply.github.com>
2021-01-18 17:53:41 +0700

    version 11
    error sqlite3 can not install and can not create database

--- my-stock-workshop/backend/api_authen.js
+++ my-stock-workshop/backend/api_authen.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const user = require("./models/user");
 
 router.post("/login", (req, res)=>{
     console.log("login: " + JSON.stringify(req.body))
